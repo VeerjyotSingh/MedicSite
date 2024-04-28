c594c52d9f356241eed23ce22fddbe19215a6816
VeerjyotSingh <103166939+VeerjyotSingh@users.noreply.github.com>
2024-04-29 01:30:53 +0530

    made files for webpages

--- main.py
+++ main.py
@@ -3,8 +3,33 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def about():
     return render_template('index.html')
+
+@app.route("/news")
+def news():
+    return render_template('news.html')
+
+@app.route("/meddit")
+def meddit():
+    return render_template('meddit.html')
+
+@app.route("/locator")
+def locator():
+    return render_template('locator.html')
+
+@app.route("/skincancer")
+def skincancer():
+    return render_template('skincancer.html')
+
+@app.route("/lungcancer")
+def lungcancer():
+    return render_template('lungcancer.html')
+
+@app.route("/breastcancer")
+def breastcancer():
+    return render_template('breastcancer.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
