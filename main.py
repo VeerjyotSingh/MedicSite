80b6d73c6b3ff2744759955375add99e6c5c16f5
VeerjyotSingh <103166939+VeerjyotSingh@users.noreply.github.com>
2024-04-30 16:47:51 +0530

    added the form

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ def locator():
 def skincancer():
     return render_template('skincancer.html')
 
-@app.route("/lungcancer")
-def lungcancer():
-    return render_template('lungcancer.html')
-
 @app.route("/breastcancer")
 def breastcancer():
     return render_template('breastcancer.html')
@@ -37,6 +33,43 @@ def contact():
 @app.route("/test")
 def test():
     return render_template("onlinetest.html")
+
+#handling lung cancer
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['USER_INFO_FILE'] = 'user_info.txt'
+
+# Mapping each step to its corresponding recording duration
+RECORDING_DURATIONS = {
+    'Breathing Deep': 11,
+    'Breathing Shallow': 8,
+    'Cough Heavy': 5,
+    'Cough Shallow': 5,
+    'Vowel A': 8,
+    'Vowel E': 8,
+    'Vowel O': 8,
+    'Counting Fast': 5,
+    'Normal': 10
+}
+
+@app.route("/lungcancer")
+def lungcancer():
+    return render_template('lungcancer.html')
+@app.route('/record', methods=['POST'])
+def record():
+    user_info = {
+        'name': request.form['name'],
+        'age': request.form['age'],
+        'place': request.form['place'],
+        'smoking': request.form['smoking'],
+        'previous_disease': request.form['previous_disease'],
+        'phone': request.form['phone'],
+        'email': request.form['email']
+    }
+    return "<h1>recording</h1>"
+
+@app.route("/disease_info_page")
+def disease_info_page():
+    return"hell yeah"
 
 if __name__ == "__main__":
     app.run(debug=True)
